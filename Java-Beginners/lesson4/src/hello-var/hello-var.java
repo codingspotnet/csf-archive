3681ae10c5c89a7cb93e5ce02b9792c9dd8adcf9
BrotherEdison <BrotherEdison@users.noreply.github.com>
2014-10-13 17:00:36 +0800

    Create hello-var.java

---
+++ Java-Beginners/lesson4/src/hello-var/hello-var.java
@@ -0,0 +1,17 @@
+package hello-var;
+
+public class HelloWorld-Variables {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        int x = 9;       //Declaring a value for x, y and adding them together
+        int y = 1;
+        int xy = x + y;
+       
+        System.out.println("Hello");  //Notice how you need to add quotation marks for text? 
+                                      //You don't need to do so for variables
+        System.out.println("Sum: "+xy); //Blending Text along with a variable
+    }
+
+}
